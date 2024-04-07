27a24a3223affd74f9ca9d6e28b34036d2a7a179
shirinzarqaa <shirinzarqaa354@gmail.com>
2024-04-07 16:57:45 +0700

    initiation front-end

---
+++ src/counter/Counter.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './counterSlice'
+import { Button } from 'react-bootstrap';
+
+export default function Counter() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <Button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </Button>
+        <span>{count}</span>
+        <Button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </Button>
+      </div>
+    </div>
+  )
+}
